772854cd31d421f7caccaf76c9dfb01e49d1c18c
Scott Donaldson <scott.p.donaldson@gmail.com>
2015-04-22 16:16:05 -0400

    cylinder, more materials

--- T.js
+++ T.js
@@ -6,26 +6,31 @@ T.box = function(x, y, z) {
     return new THREE.BoxGeometry(x, y, z);
 };
 
+T.cylinder = function(rTop, rBottom, h, seg) {
+    return new THREE.CylinderGeometry(rTop, rBottom, h, seg);
+}
+
 T.material = function(type, attrs) {
 
     var types = {
         basic: 'MeshBasicMaterial',
-        lambert: 'MeshLambertMaterial'
+        lambert: 'MeshLambertMaterial',
+        depth: 'MeshDepthMaterial',
+        phong: 'MeshPhongMaterial'
     };
 
-    if (type in types) {
+    if ( type in types ) {
         // include attributes
-        if ( typeof attrs === 'object' ) {
-            // default to white
-            if (!attrs.color) attrs.color = '#fff';
-            return new THREE[types[type]](attrs);
+        if ( typeof attrs === 'object' && !attrs.color ) {
+            // attrs.color = '#fff';
         // or, if a string, then it's the color
         } else if (typeof attrs === 'string') {
-            return new THREE[types[type]]({ color: attrs });
+            attrs = { color: attrs };
         } else if ( !attrs ) {
             attrs = { color: '#fff' };
-            return new THREE[types[type]](attrs);
         }
+
+        return new THREE[types[type]](attrs);
     }
 };
 
