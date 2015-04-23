27e57862d95cb149485124af3f8bcb8539a30927
Scott Donaldson <scott.p.donaldson@gmail.com>
2015-04-23 00:21:52 -0400

    T is now a badass constructor

--- T.js
+++ T.js
@@ -1,16 +1,66 @@
-function T() {
+T = function() {
 
+    var scene, camera, renderer;
+
+    var WIDTH = window.innerWidth,
+        HEIGHT = window.innerHeight,
+        VIEW_ANGLE = 45,
+        ASPECT = WIDTH / HEIGHT,
+        NEAR = 0.1,
+        FAR = 100000;
+
+    renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        shadowMapEnabled: true
+    });
+
+    camera = new THREE.PerspectiveCamera(
+        VIEW_ANGLE,
+        ASPECT,
+        NEAR,
+        FAR
+    );
+
+    scene = new THREE.Scene;
+    scene.add(camera);
+
+    renderer.setSize(WIDTH, HEIGHT);
+    document.body.appendChild(renderer.domElement);
+
+    this.scene = scene;
+    this.camera = normalize(camera);
+    this.renderer = renderer;
+
+    return this;
 }
 
-T.box = function(x, y, z) {
-    return new THREE.BoxGeometry(x, y, z);
+function shadows(thing) {
+    thing.castShadow = true;
+    thing.receiveShadow = true;
+    return thing;
+}
+
+function normalize(thing) {
+    ['x', 'y', 'z'].forEach(function(d) {
+        var a = d;
+        thing[d] = function(n) {
+            this.position[a] = n;
+        };
+    });
+    return thing;
 };
 
-T.cylinder = function(rTop, rBottom, h, seg) {
-    return new THREE.CylinderGeometry(rTop, rBottom, h, seg);
+Box = function(x, y, z) {
+    var box = new THREE.BoxGeometry(x, y, z);
+    return shadows(box);
+};
+
+Cylinder = function(rTop, rBottom, h, seg) {
+    var cylinder = new THREE.CylinderGeometry(rTop, rBottom, h, seg);
+    return shadows(cylinder);
 }
 
-T.material = function(type, attrs) {
+Material = function(type, attrs) {
 
     var types = {
         basic: 'MeshBasicMaterial',
@@ -34,6 +84,32 @@ T.material = function(type, attrs) {
     }
 };
 
-T.mesh = function(geo, material) {
-    return new THREE.Mesh(geo, material);
+T.prototype.mesh = function(geo, material) {
+    var mesh = new T.Mesh(geo, material, this);
+    return mesh;
 };
+
+T.prototype.light = function() {
+    var light = new THREE.DirectionalLight('#fff');
+    light.castShadow = true;
+    this.scene.add(light);
+    return normalize(light);
+};
+
+T.Mesh = function(geo, material, world) {
+    var mesh = new THREE.Mesh(geo, material);
+    world.scene.add(mesh);
+    return normalize(mesh);
+};
+
+T.prototype.render = function(cb) {
+    var t = 0,
+        _this = this;
+    function render() {
+        _this.renderer.render(_this.scene, _this.camera);
+        cb.bind(_this)(t);
+        t++;
+        requestAnimationFrame(render);
+    }
+    render();
+}
